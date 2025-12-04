1f86b5c0ecc8edf6324046678be536a8fe794fe1
Gallie83 <keving2913@hotmail.com>
2025-12-04 15:03:41 +0000

    Build to docs folder for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-
 import { defineConfig } from "vite";
 import path from "path";
 import react from "@vitejs/plugin-react";
@@ -6,6 +5,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: './' ,
   plugins: [react()],
+  build: {
+    outDir: 'docs'
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
